f6abbd8bbe31c7ff4d9a441db63faf90e404cba0
codethecodeman <3502286+codethecodeman@users.noreply.github.com>
2024-08-29 13:25:05 -0400

    Add ICannoliModule

---
+++ CannoliKit/Interfaces/ICannoliModule.cs
@@ -0,0 +1,18 @@
+﻿using CannoliKit.Modules;
+
+namespace CannoliKit.Interfaces
+{
+    /// <summary>
+    /// Represents a Cannoli Module.
+    /// </summary>
+    public interface ICannoliModule
+    {
+        /// <summary>
+        /// Build the module into components that can be applied to a Discord message.
+        /// </summary>
+        /// <returns>
+        /// Cannoli module components.
+        /// </returns>
+        public Task<CannoliModuleComponents> BuildComponentsAsync();
+    }
+}
