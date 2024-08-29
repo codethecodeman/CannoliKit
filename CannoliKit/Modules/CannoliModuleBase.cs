c9e4e22d0c67358e3aac0296833ec3d05d6b5d98
CodeMan <3502286+codethecodeman@users.noreply.github.com>
2024-08-29 13:26:08 -0400

    Add ICannoliModule (#6)

--- CannoliKit/Modules/CannoliModuleBase.cs
+++ CannoliKit/Modules/CannoliModuleBase.cs
@@ -1,20 +1,18 @@
-﻿using CannoliKit.Models;
+﻿using CannoliKit.Interfaces;
+using CannoliKit.Models;
 
 namespace CannoliKit.Modules
 {
     /// <summary>
     /// Represents a base class for a Cannoli Module.
     /// </summary>
-    public abstract class CannoliModuleBase
+    public abstract class CannoliModuleBase : ICannoliModule
     {
         internal CannoliModuleBase() { }
 
         /// <summary>
-        /// Build the module into components that can be applied to a Discord message.
+        /// <inheritdoc/>
         /// </summary>
-        /// <returns>
-        /// Cannoli module components.
-        /// </returns>
         public abstract Task<CannoliModuleComponents> BuildComponentsAsync();
         internal abstract Task LoadModuleState(CannoliRoute route);
         internal abstract Task SaveModuleState();
